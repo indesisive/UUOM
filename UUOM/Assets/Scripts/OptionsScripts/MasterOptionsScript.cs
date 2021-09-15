b06125186ba73607ddea6ba4703a0f82d2a2e55d
LordShadow-itch-io <samueloloy13@gmail.com>
2021-09-15 22:46:15 +0100

    BETA.v.0.1

--- UUOM/Assets/Scripts/OptionsScripts/MasterOptionsScript.cs
+++ UUOM/Assets/Scripts/OptionsScripts/MasterOptionsScript.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering.Universal;
 
 public class MasterOptionsScript : MonoBehaviour
 {
+    private string cantFindElementAttributes = "Cannot find element Attributes, make sure everything is spelt correctly, or the name, type and tab int connect!";
+
+    //public GameObject screenContent;
+
+    //[HideInInspector]
+    public List<GameObject> contentParents = new List<GameObject>();
+    public List<GameObject> tabButtons = new List<GameObject>();
+
+    public GameObject boolTemplate;
+    public GameObject dropdownTemplate;
+    public GameObject sliderTemplate;
+
+    [Space]
+    [Header("Loose Values")]
+    public GameObject greyScaleColorCurves;
+
     [Header("Element Lists:")]
     [Space]
     //[("Remember that elementName #0 goes with elementType #0 and elementTab #0, don't mess it up!")]
@@ -12,16 +31,140 @@ public class MasterOptionsScript : MonoBehaviour
     public List<string> elementTypes = new List<string>();
     public List<int> elementTabs = new List<int>();
     [Space]
-    [Header("Create element:")]
+    [Header("Simple Create Element:")]
     [Space]
+    [HideInInspector]
     public string elementName;
+    [HideInInspector]
     public string elementType;
+    [HideInInspector]
     public int elementTab;
+    [Space]
+    [Header("Simple Remove Element:")]
+    [Space]
+    [HideInInspector]
+    public string elementNameToRemove;
+    [HideInInspector]
+    public string elementTypeToRemove;
+    [HideInInspector]
+    public int elementTabToRemove;
 
     public void createElement()
     {
         elementNames.Add(elementName);
         elementTypes.Add(elementType);
         elementTabs.Add(elementTab);
+    }
+
+    public void removeElement()
+    {
+        bool found = false;
+
+        for (int i = 0; i < elementNames.Count; i++)
+        {
+            if (elementNames[i] == elementNameToRemove && elementTypes[i] == elementTypeToRemove && elementTabs[i] == elementTabToRemove)
+            {
+                /*for (int j = 0; j < elementTypes.Count; j++)
+                {
+                    if (elementTypes[j] == elementTypeToRemove)
+                    {
+                        for (int l = 0; l < elementTabs.Count; l++)
+                        {
+                            if (elementTabs[l] == elementTabToRemove)
+                            {
+                                elementNames.RemoveAt(i);
+                                elementTypes.RemoveAt(j);
+                                elementTabs.RemoveAt(l);
+                            }
+                        }
+                    }
+                }*/
+
+                elementNames.RemoveAt(i);
+                elementTypes.RemoveAt(i);
+                elementTabs.RemoveAt(i);
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Debug.LogError(cantFindElementAttributes);
+        }
+        
+    }
+
+    public void updateOptions()
+    {
+
+
+        /*for (int i = 0; i <= screenContent.transform.childCount; i++)
+        {
+            DestroyImmediate(screenContent.GetComponent<RectTransform>().GetChild(i).gameObject);
+        }*/
+
+        for (int i = 0; i < contentParents.Count; i++)
+        {
+            foreach (Transform child in contentParents[i].transform)
+            {
+                DestroyImmediate(child.gameObject);
+            }
+        }
+
+        for (int i = 0; i < elementNames.Count; i++)
+        {
+            //if (elementTabs[i] == 0)
+            //{
+                if (!contentParents[elementTabs[i]].transform.Find(elementNames[i]))
+                {
+                    if (elementTypes[i] == "Bool")
+                    {
+                        GameObject newOptionOBJ = Instantiate(boolTemplate, contentParents[elementTabs[i]].gameObject.transform);
+                        newOptionOBJ.name = elementNames[i];
+                        newOptionOBJ.transform.Find("Name").GetComponent<TMP_Text>().text = elementNames[i];
+                    }
+
+                    if(elementTypes[i] == "Dropdown")
+                    {
+                        GameObject newOptionOBJ = Instantiate(dropdownTemplate, contentParents[elementTabs[i]].gameObject.transform);
+                        newOptionOBJ.name = elementNames[i];
+                        newOptionOBJ.transform.Find("Name").GetComponent<TMP_Text>().text = elementNames[i];
+                    }
+
+                if (elementTypes[i] == "Slider")
+                {
+                    GameObject newOptionOBJ = Instantiate(sliderTemplate, contentParents[elementTabs[i]].gameObject.transform);
+                    newOptionOBJ.name = elementNames[i];
+                    newOptionOBJ.transform.Find("Name").GetComponent<TMP_Text>().text = elementNames[i];
+                }
+            }
+           // }
+        }
+    }
+
+    //Buttons\\
+    public void screenButton()
+    {
+        for (int i = 0; i < tabButtons.Count; i++)
+        {
+            tabButtons[i].SetActive(false);
+        }
+        tabButtons[0].SetActive(true);
+    }
+
+    public void audioButton()
+    {
+        for (int i = 0; i < tabButtons.Count; i++)
+        {
+            tabButtons[i].SetActive(false);
+        }
+        tabButtons[1].SetActive(true);
+    }
+
+    //Option Functions\\
+
+    public void greyScale(bool uiValue)
+    {
+        greyScaleColorCurves.SetActive(uiValue);
     }
 }
--- UUOM/Assets/Scripts/OptionsScripts/OptionsInspectorEditor.cs
+++ UUOM/Assets/Scripts/OptionsScripts/OptionsInspectorEditor.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEditor;
+using UnityEditor.Rendering.Universal;
 
 [CustomEditor(typeof(MasterOptionsScript))]
 public class OptionsInspectorEditor : Editor
@@ -8,9 +10,57 @@ public class OptionsInspectorEditor : Editor
     {
         DrawDefaultInspector();
         MasterOptionsScript mstr = (MasterOptionsScript)target;
-        if(GUILayout.Button("Create Element"))
+        //base.OnInspectorGUI();
+        
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Update Options"))
+        {
+            mstr.updateOptions();
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Simple Create Element:", EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.LabelField("Element Name",GUILayout.MaxWidth(85));
+        mstr.elementName = EditorGUILayout.TextField(mstr.elementName);
+
+        EditorGUILayout.LabelField("Element Type", GUILayout.MaxWidth(85));
+        mstr.elementTypeToRemove = EditorGUILayout.TextField(mstr.elementType);
+
+        EditorGUILayout.LabelField("Element Tab", GUILayout.MaxWidth(77));
+        mstr.elementTab = EditorGUILayout.IntField(mstr.elementTab);
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Create Element"))
         {
             mstr.createElement();
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Simple Remove Element:",EditorStyles.boldLabel);
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.LabelField("Element Name", GUILayout.MaxWidth(85));
+        mstr.elementNameToRemove = EditorGUILayout.TextField(mstr.elementNameToRemove);
+
+        EditorGUILayout.LabelField("Element Type", GUILayout.MaxWidth(80));
+        mstr.elementTypeToRemove = EditorGUILayout.TextField(mstr.elementTypeToRemove);
+
+        EditorGUILayout.LabelField("Element Tab", GUILayout.MaxWidth(77));
+        mstr.elementTabToRemove = EditorGUILayout.IntField(mstr.elementTabToRemove);
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Remove Element"))
+        {
+            mstr.removeElement();
         }
     }
 }
